4e9fcf6dcad4b8c708799b7e59c5fba8c88c594e
Veronica <veronica.cernenchii@mail.ru>
2022-10-01 23:37:48 +0300

    add new folders

---
+++ Syminar2/ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Random rnd = new Random(); /*Создаем отдельный рандом*/
+// int num = rnd.Next(10, 158);
+
+using static System.Console;
+Clear();
+
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+
+int a0 = number%10;
+//int a1 = number/10;
+int a2 = number/100;
+
+int result = a2*10+a0;
+WriteLine(result);
---
+++ Syminar2/ex007/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+// WriteLine(" Введите количество разрядов n --> ");
+// int n = Convert.ToInt32(ReadLine());
+// int number = new Random().Next(10*(n-1),100*());
+// WriteLine(number);
+
+WriteLine(" Введитеи двухзначное число --> ");
+int num1 = Convert.ToInt32(ReadLine());
+WriteLine($" Ваше число --> ");
+int num2 =0;
+while(num2<= num1){
+ Write($"{num2},");
+ num2++;
+}
