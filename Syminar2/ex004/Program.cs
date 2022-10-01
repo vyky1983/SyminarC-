3396cbf58c132b5b6701682781ec432eb31a7e0a
Veronica <veronica.cernenchii@mail.ru>
2022-10-01 23:09:07 +0300

    add new folder

---
+++ Syminar2/ex004/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+int num1 = int.Parse(ReadLine()!);
+
+if (num1 == 0)
+{
+    WriteLine("Просто ");
+    return;
+}
+int div1 = num1 % 7;
+int div2 = num1 % 23;
+
+// if (div1 == 0 && div2 == 0)
+// {
+//     WriteLine($"{num1} кратно 7 и 23");
+// }
+// else
+// {
+//     WriteLine($" Число {num1} не кратно 7 и 23");
+// }
+// string answer =num1%7==0&&num1%23==0?"Кратное":"Некратное";
+// WriteLine(answer);
+WriteLine(num1%7==0&&num1%23==0? /*Тернарный оператор*/"Кратное":"Некратное");
+
---
+++ Syminar2/ex009/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+Write("Введите число: ");
+int num = Convert.ToInt32(ReadLine());
+WriteLine($" Ваше число {num} ");
+// if(num%2==0){
+//  WriteLine("Четное");
+// }
+// else {
+//  WriteLine(" Число нечетное");
+// }
+// string answer = num%2==0?"Четное" : "Нечетное";
+// WriteLine(answer);
+WriteLine(num%2==0?" Четное ": "Нечетное" );
