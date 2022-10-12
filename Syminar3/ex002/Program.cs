7c94cffe4853197a1e1c33434a9604444896631f
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-12 13:29:37 +0300

    new folder

--- Syminar3/ex002/Program.cs
+++ Syminar3/ex002/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+int number  = new Random().Next(100,1000);
+int a0=number%10;
+//int a1=number/10;
+int a2=number/100;
+int result=a2*10+a0;
+WriteLine(result);
+ //WriteLine($"{a2}{a0}");
