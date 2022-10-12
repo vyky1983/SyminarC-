b9239c6d297d2a45388b748e895b025bc40469d8
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-12 13:51:01 +0300

    add new information

--- Syminar3/ex003/Program.cs
+++ Syminar3/ex003/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(ReadLine());
+
+if (num2 == 0)
+{
+    WriteLine("На ноль делить нельзя");
+}
+else
+{
+    int div = num1 % num2;
+    if (div == 0)
+    {
+        WriteLine($"{num1} кратно {num2}");
+    }
+    else
+    {
+        WriteLine($"Остаток от деления {num1} на {num2}: {div} ");
+    }
+}
