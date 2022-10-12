0abc52eac1c24aaa7362cca0d1bd9135c741d167
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-12 13:03:57 +0300

    add new information ex001

--- Syminar3/ex001/Program.cs
+++ Syminar3/ex001/Program.cs
@@ -1,16 +1,19 @@
 ﻿using static System.Console;
+
 Clear();
 
-//Write(" Ведите число");
-int rnd = new Random().Next(10,100+1);
+Write(" Ведите число");
+int rnd = new Random().Next(10, 100 + 1);
 WriteLine(rnd);
-int a0 = rnd%10;
-int a1 = rnd/10;
-if(a0>a1)
+int a0 = rnd % 10;
+int a1 = rnd / 10;
+if (a0 > a1)
 {
- WriteLine($" Максимальное число = {a0} ");
+    WriteLine($" Максимальное число = {a0} ");
 }
-else{
- WriteLine(a1);
+else
+{
+    WriteLine(a1);
 }
-//WriteLine(a0>a1?a0:a1);
+//WriteLine(a0>a1?a0:a1);  /*Тернарный оператор*/
+
