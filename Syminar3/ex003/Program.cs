b479fd0435de33f0363fe967cf63f36f9818b21b
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-12 14:10:05 +0300

    ex003

--- Syminar3/ex003/Program.cs
+++ Syminar3/ex003/Program.cs
@@ -7,6 +7,13 @@ int num1 = Convert.ToInt32(ReadLine());
 WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(ReadLine());
 
+if(num2>num1)
+{
+ int temp =num1; /*Меняем местами переменные*/
+ num1=num2;
+ num2=temp;
+}
+
 if (num2 == 0)
 {
     WriteLine("На ноль делить нельзя");
