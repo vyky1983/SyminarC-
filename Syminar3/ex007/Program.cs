5eff115cf40154654a0d9fff2763aa467fda760b
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-13 19:14:17 +0300

    Add Sym3 ex 007, ex008

---
+++ Syminar3/ex007/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("1 стрелок : ");
+int num1 = Convert.ToInt32(ReadLine());
+WriteLine("1 стрелок : ");
+int num2 = Convert.ToInt32(ReadLine());
+WriteLine($"1 стрелок не выбил {num2 - 1} банок");
+Write($"2 стрелок не выбил {num1-1}  банок");
