903ad52d026c715676ff57c9bd8e2d3914d696fb
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-13 20:00:12 +0300

    modified:   ex008/

--- Syminar3/ex008/Program.cs
+++ Syminar3/ex008/Program.cs
@@ -1,2 +1,9 @@
 ﻿using static System.Console;
+
 Clear();
+
+System.Console.WriteLine("Введитем количество сделанных корабликов");
+int num = Convert.ToInt32(ReadLine());
+var ship = num / 6;
+WriteLine($"Девочка сделала {ship * 4} корабликов ");
+WriteLine($"Каждый мальчик сделал по {ship} корабликов ");
