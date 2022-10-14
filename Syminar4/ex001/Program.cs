a77fd4a90c08aee16f8b7078963cdb09f7b2d0ec
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-14 16:12:10 +0300

    EX003, ex004 Sym4

--- Syminar4/ex001/Program.cs
+++ Syminar4/ex001/Program.cs
@@ -1,2 +1,22 @@
 ﻿using static System.Console;
+
 Clear();
+
+/*Hапишите прграмму, каторая реализуетметод который принимает число "A" и возвращает сумму чисел от 1 до  "A" */
+
+Write("Введите номер A: ");
+int num1 = Convert.ToInt32(ReadLine());
+
+int sum = GetSum(num1);
+WriteLine($"Сумма чисел от 1 до А = {sum}");
+
+int GetSum(int numA)
+{
+    int result = 0;
+    while(numA>0)
+    {
+     result+=numA;
+     numA--;
+    }
+    return result;
+}
--- Syminar4/ex002/Program.cs
+++ Syminar4/ex002/Program.cs
@@ -1,2 +1,21 @@
 ﻿using static System.Console;
+
 Clear();
+
+/*Hапишите прграмму, каторая реализуетметод который принимает число "A" и возвращает сумму чисел от 1 до  "A" */
+
+Write("Введите номер A: ");
+int num1 = Convert.ToInt32(ReadLine());
+
+int sum = GetSum(num1);
+WriteLine($"Сумма чисел от 1 до А = {sum}");
+
+int GetSum(int numA)
+{
+    int result = 0;
+    for (int i = 1; i <= numA; i++)
+    {
+        result += i;
+    }
+    return result;
+}
