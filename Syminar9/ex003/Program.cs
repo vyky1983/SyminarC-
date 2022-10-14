b2d3cbba221eeed077542173606a9ed4b425f8c9
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-14 14:02:36 +0300

    new inf

---
+++ Syminar9/ex003/Program.cs
@@ -0,0 +1,17 @@
+﻿/***Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+
+45 -> 9
+*/
+// ```string GetNumbersNM2(int n, int m)
+// {
+//     if (n == m)
+//     {
+//         return n.ToString();    
+//     }
+//     return (n > m) ? GetNumbersNM2(n - 1, m) + " " + n : GetNumbersNM2(m - 1, n) + " " + m;
+// }```
+// int SumOfDigits(int n){
+//     return (n/10 == 0) ? n%10 : SumOfDigits((n)/10) + n%10;
+// }
