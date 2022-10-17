75939105cfdcf6693b916fec595ffb795c948047
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-17 16:01:48 +0300

    Add Sym ex007, ex008

--- Syminar4/ex005/Program.cs
+++ Syminar4/ex005/Program.cs
@@ -1,25 +1,13 @@
 ﻿using static System.Console;
 Clear();
-System.Console.WriteLine("Введите номер: ");
-int num = Convert.ToInt32(ReadLine());
-int fact =GetFactorial(num);
-System.Console.WriteLine(fact);
+System.Console.WriteLine(" ");
 
 int GetFactorial(int num1)
 {
- int result=1;
 for (int i = 1; i <= num1; i++)
 {
-result*=i;
+int result=result*i;
 
 }
 return result;
 }
-
-// int GetFactor(int number){
-// int result = 1;
-// for(int i = 1; i <= number; i++){
-// result *= i;
-// }
-// return result;
-// }
