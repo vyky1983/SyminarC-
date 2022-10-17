e2f40c59575de4366b78b54dddbde08016750178
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-17 15:04:13 +0300

    ex005, 006

--- Syminar4/ex004/Program.cs
+++ Syminar4/ex004/Program.cs
@@ -1,4 +1,40 @@
 ﻿using static System.Console;
+
 Clear();
 
+/*Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+456 -> 3
+
+78 -> 2
+
+89126 -> 5*/
+
+WriteLine("Введите число ");
+int num = Convert.ToInt32(ReadLine());
+int res = GetLen(num);
+System.Console.WriteLine(res);
+
+int GetLen(int num1)
+{
+    int result1 = 0;
+    while (num1 > 0)
+    {
+        num1 /= 10;
+        result1++;
+    }
+    return result1;
+}
+
+// int GetLen(int num1)
+// {
+//     int result1 = 0;
+//     for (int i = 0; i <= num1; i++)
+//     {
+//      num1/=10;
+//      result1++;
+     
+//     }
+//     return result1;
+// }
 
---
+++ Syminar4/ex005/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+Clear();
+System.Console.WriteLine("Введите номер: ");
+int num = Convert.ToInt32(ReadLine());
+int fact =GetFactorial(num);
+System.Console.WriteLine(fact);
+
+int GetFactorial(int num1)
+{
+ int result=1;
+for (int i = 1; i <= num1; i++)
+{
+result*=i;
+
+}
+return result;
+}
+
+// int GetFactor(int number){
+// int result = 1;
+// for(int i = 1; i <= number; i++){
+// result *= i;
+// }
+// return result;
+// }
